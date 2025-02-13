d382a92b092c9b930952f745232703f8edff9ad6
Arthur <formaformosa@gmail.com>
2025-02-13 17:23:48 -0300

    commit letra b)

---
+++ b/b/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace b
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Digite uma palavra: ");
+            string palavra = Console.ReadLine();
+
+            Console.WriteLine("A quantidade de caractéres na palavra é: {0}", quantCaracteres(palavra));
+
+            Console.ReadKey(true);
+        }
+
+        public static int quantCaracteres (string palavra)
+        {
+            if (palavra == null)
+            {
+                return 0;
+            }
+
+            // Retorna o número de caractéres na string
+            return palavra.Length;
+           
+        }
+    }
+}
