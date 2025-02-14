c8b80cf2e8765876b73684ef3ebba4654e2cf690
Arthur <formaformosa@gmail.com>
2025-02-14 12:49:47 -0300

    commit letra c

---
+++ c/c/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o primeiro número inteiro: ");
+        int primeiroNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número inteiro: ");
+        int segundoNum = int.Parse(Console.ReadLine());
+
+        // Exibe a soma dos números
+        Console.WriteLine($"A soma de {primeiroNum} + {segundoNum} é: {Soma(primeiroNum, segundoNum)}");
+        Console.ReadKey(true);
+    }
+
+    // Método que retorna a soma dos números
+    public static int Soma (int primeiroNum, int segundoNum)
+    {
+        int somaNumeros = primeiroNum + segundoNum;
+
+        return somaNumeros;
+    }
+}
