f42c1f9f322d6d1a4439c358b3df630c16d755c2
Arthur <formaformosa@gmail.com>
2025-02-14 13:28:11 -0300

    commit letra g)

---
+++ g/g/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma string: ");
+        string entrada = Console.ReadLine();
+
+        string resultado = ObterCaracteresImpares(entrada);
+        Console.WriteLine("A nova string com caracteres em posições ímpares é: " + resultado);
+    }
+
+    static string ObterCaracteresImpares(string str)
+    {
+        if (str == null)
+        {
+            throw new ArgumentNullException(nameof(str), "A string não pode ser nula.");
+        }
+
+        string resultado = "";
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            // Verifica se a posição é ímpar (considerando 0 como par)
+            if (i % 2 != 0)
+            {
+                resultado += str[i];
+            }
+        }
+
+        return resultado;
+    }
+}
