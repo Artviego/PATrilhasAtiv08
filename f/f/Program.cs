af63a40560c5a2b6da56d36114303673de3ae51a
Arthur <formaformosa@gmail.com>
2025-02-14 13:20:58 -0300

    commit letra e)

---
+++ f/f/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número inteiro: ");
+        string input = Console.ReadLine();
+        int numero;
+
+        if (int.TryParse(input, out numero))
+        {
+            Console.WriteLine(ParOuImpar(numero));
+        }
+        else
+        {
+            Console.WriteLine("\nEntrada inválida! Tente novamente.");
+        }
+
+    }
+
+    public static int ParOuImpar(int numero)
+    {
+        if (numero % 2 == 0)
+        {
+            Console.Write("\nO número é verdadeiro(par): ");
+        }
+        else
+        {
+            Console.Write("\nO número á falso(ímpar): ");
+        }
+        return numero;
+    }
+
+
+}
