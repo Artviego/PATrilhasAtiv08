42aefbbf5355793f1f61d27a09626eb0514609da
Arthur <formaformosa@gmail.com>
2025-02-15 19:53:52 -0300

    commit letra i)

--- i/i/Program.cs
+++ i/i/Program.cs
@@ -2,6 +2,33 @@
 
 public class Program
 {
+    public static void Main()
+    {
+        Console.Write("Digite um número inteiro para verificar se é primo: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int numero))
+        {
+            bool resultado = VerificarPrimo(numero);
+
+            if (resultado)
+            {
+                Console.WriteLine($"\n{numero} é um número primo.");
+            }
+            else
+            {
+                Console.WriteLine($"\n{numero} não é um número primo.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("\nPor favor, insira um número válido.");
+        }
+
+        Console.ReadKey(true);
+    }
+
+    // Método que verifica se o número é primo
     public static bool VerificarPrimo(int numero)
     {
         if (numero <= 1) // Números menores ou iguais a 1 não são primos
@@ -18,29 +45,5 @@ public class Program
         }
 
         return true; // Se não encontrou divisores, é primo
-    }
-
-    public static void Main()
-    {
-        Console.Write("Digite um número inteiro para verificar se é primo: ");
-        string input = Console.ReadLine();
-
-        if (int.TryParse(input, out int numero))
-        {
-            bool resultado = VerificarPrimo(numero);
-
-            if (resultado)
-            {
-                Console.WriteLine($"{numero} é um número primo.");
-            }
-            else
-            {
-                Console.WriteLine($"{numero} não é um número primo.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Por favor, insira um número válido.");
-        }
     }
 }
